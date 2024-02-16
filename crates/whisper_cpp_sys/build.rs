385f2a500bf76b499991964cfb5f7ea790947edf
Pedro Valente <pedro.amaral.valente@gmail.com>
2024-02-16 16:22:52 +0000

    wip: cuda execution

--- crates/whisper_cpp_sys/build.rs
+++ crates/whisper_cpp_sys/build.rs
@@ -15,7 +15,7 @@ fn main() {
     let mut config = cmake::Config::new(submodule_dir);
 
     config
-        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_SHARED_LIBS", "ON")
         .define("WHISPER_BUILD_EXAMPLES", "OFF")
         .define("WHISPER_BUILD_TESTS", "OFF")
         .define("WHISPER_NO_ACCELERATE", "ON") // TODO accelerate is used by default, but is causing issues atm, check why
@@ -49,15 +49,12 @@ fn main() {
 
     let dst = config.build();
 
+    println!("cargo:rustc-link-search=native={}/bin", dst.display());
     println!(
         "cargo:rustc-link-search=native={}/lib/static",
         dst.display()
     );
-    println!(
-        "cargo:rustc-link-search=native={}/lib64/static",
-        dst.display()
-    );
-    println!("cargo:rustc-link-lib=static=whisper");
+    println!("cargo:rustc-link-lib=dylib=whisper");
 
     let bindings = bindgen::Builder::default()
         .header(submodule_dir.join("ggml.h").to_string_lossy())
@@ -86,34 +83,22 @@ fn main() {
 
 #[cfg(feature = "compat")]
 mod compat {
-    use std::path::Path;
+    use std::collections::HashSet;
+    use std::env;
+    use std::fmt::{Display, Formatter};
+    use std::path::{Path, PathBuf};
     use std::process::Command;
 
     pub fn redefine_symbols(out_path: impl AsRef<Path>) {
-        // TODO this whole section is a bit hacky, could probably clean it up a bit, particularly the retrieval of symbols from the library files
-        // TODO do this for cuda if necessary
-
         let whisper_lib_name = lib_name();
-        let (nm_name, objcopy_name) = tool_names();
-        println!("Modifying {whisper_lib_name}, symbols acquired via \"{nm_name}\" and modified via \"{objcopy_name}\"");
+        let (nm, objcopy) = tools();
+        println!("Modifying {whisper_lib_name}, symbols acquired via \"{nm}\" and modified via \"{objcopy}\"");
 
-        let lib_path = out_path.as_ref().join("lib").join("static");
+        let lib_path = out_path.as_ref().join("bin");
 
         // Modifying symbols exposed by the ggml library
 
-        let output = Command::new(nm_name)
-            .current_dir(&lib_path)
-            .arg(whisper_lib_name)
-            .args(["-p", "-P"])
-            .output()
-            .expect("Failed to acquire symbols from the compiled library.");
-        if !output.status.success() {
-            panic!(
-                "An error has occurred while acquiring symbols from the compiled library ({})",
-                output.status
-            );
-        }
-        let out_str = String::from_utf8_lossy(output.stdout.as_slice());
+        let out_str = nm_symbols(&nm, whisper_lib_name, &lib_path);
         let symbols = get_symbols(
             &out_str,
             [
@@ -139,91 +124,210 @@ mod compat {
                 },
             ],
         );
-
-        let mut cmd = Command::new(objcopy_name);
-        cmd.current_dir(&lib_path);
-        for symbol in symbols {
-            cmd.arg(format!("--redefine-sym={symbol}=whisper_{symbol}"));
-        }
-        let status = cmd
-            .arg(whisper_lib_name)
-            .status()
-            .expect("Failed to modify global symbols from the ggml library.");
-        if !status.success() {
-            panic!(
-                "An error as occurred while modifying global symbols from library file ({})",
-                status
-            );
-        }
+        objcopy_redefine(&objcopy, whisper_lib_name, "whisp_", symbols, &lib_path);
     }
 
     /// Returns *Whisper.cpp*'s compiled library name, based on the operating system.
     fn lib_name() -> &'static str {
         if cfg!(target_family = "windows") {
-            "whisper.lib"
+            "whisper.dll"
         } else if cfg!(target_family = "unix") {
-            "libwhisper.a"
+            "libwhisper.so"
         } else {
             println!("cargo:warning=Unknown target family, defaulting to Unix lib names");
-            "libwhisper.a"
+            "libwhisper.so"
         }
     }
 
-    /// Returns the names of tools equivalent to [nm][nm] and [objcopy][objcopy].
+    /// Returns [`Tool`]s equivalent to [nm][nm] and [objcopy][objcopy].
     ///
     /// [nm]: https://www.man7.org/linux/man-pages/man1/nm.1.html
     /// [objcopy]: https://www.man7.org/linux/man-pages/man1/objcopy.1.html
-    fn tool_names() -> (&'static str, &'static str) {
+    fn tools() -> (Tool, Tool) {
         let nm_names;
         let objcopy_names;
-        if cfg!(target_family = "unix") {
+        let nm_help;
+        let objcopy_help;
+        if cfg!(target_os = "linux") {
             nm_names = vec!["nm", "llvm-nm"];
             objcopy_names = vec!["objcopy", "llvm-objcopy"];
+            nm_help = vec!["\"nm\" from GNU Binutils", "\"llvm-nm\" from LLVM"];
+            objcopy_help = vec![
+                "\"objcopy\" from GNU Binutils",
+                "\"llvm-objcopy\" from LLVM",
+            ];
+        } else if cfg!(any(
+            target_os = "macos",
+            target_os = "ios",
+            target_os = "dragonfly"
+        )) {
+            nm_names = vec!["nm", "llvm-nm"];
+            objcopy_names = vec!["llvm-objcopy"];
+            nm_help = vec!["\"llvm-nm\" from LLVM 17"];
+            objcopy_help = vec!["\"llvm-objcopy\" from LLVM 17"];
         } else {
             nm_names = vec!["llvm-nm"];
             objcopy_names = vec!["llvm-objcopy"];
+            nm_help = vec!["\"llvm-nm\" from LLVM 17"];
+            objcopy_help = vec!["\"llvm-objcopy\" from LLVM 17"];
         }
 
-        let nm_name;
+        let nm_env = "NM_PATH";
+        println!("cargo:rerun-if-env-changed={nm_env}");
+        println!("Looking for \"nm\" or an equivalent tool");
+        let nm_name = find_tool(&nm_names, nm_env).unwrap_or_else(move || {
+            panic_tool_help("nm", nm_env, &nm_help);
+            unreachable!("The function above should have panicked")
+        });
 
-        if let Some(path) = option_env!("NM_PATH") {
-            nm_name = path;
-        } else {
-            println!("Looking for \"nm\" or an equivalent tool");
-            nm_name = find_tool(&nm_names).expect(
-                "No suitable tool equivalent to \"nm\" has been found in \
-            PATH, if one is already installed, either add it to PATH or set NM_PATH to its full path",
-            );
-        }
-
-        let objcopy_name;
-        if let Some(path) = option_env!("OBJCOPY_PATH") {
-            objcopy_name = path;
-        } else {
-            println!("Looking for \"objcopy\" or an equivalent tool");
-            objcopy_name = find_tool(&objcopy_names).expect("No suitable tool equivalent to \"objcopy\" has \
-            been found in PATH, if one is already installed, either add it to PATH or set OBJCOPY_PATH to its full path");
-        }
+        let objcopy_env = "OBJCOPY_PATH";
+        println!("cargo:rerun-if-env-changed={objcopy_env}");
+        println!("Looking for \"objcopy\" or an equivalent tool..");
+        let objcopy_name = find_tool(&objcopy_names, objcopy_env).unwrap_or_else(move || {
+            panic_tool_help("objcopy", objcopy_env, &objcopy_help);
+            unreachable!("The function above should have panicked")
+        });
 
         (nm_name, objcopy_name)
     }
 
-    /// Returns the first tool found in the system, given a list of tool names, returning the first one found and
-    /// printing its version.
+    /// A command line tool name present in `PATH` or its full [`Path`].
+    enum Tool {
+        /// The name of a tool present in `PATH`.
+        Name(&'static str),
+
+        /// The full [`Path`] to a tool.
+        FullPath(PathBuf),
+    }
+
+    impl Display for Tool {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Tool::Name(name) => write!(f, "{}", name),
+                Tool::FullPath(path) => write!(f, "{}", path.display()),
+            }
+        }
+    }
+
+    /// Returns the first [`Tool`] found in the system `PATH`, given a list of tool names, returning
+    /// the first one found and printing its version.
     ///
-    /// Returns [`Option::None`] if no tool is found.
-    fn find_tool<'a>(names: &[&'a str]) -> Option<&'a str> {
+    /// If a value is present in the provided environment variable name, it will get checked
+    /// instead.
+    ///
+    /// ## Panic
+    /// Returns [`Option::None`] if no [`Tool`] is found.
+    fn find_tool(names: &[&'static str], env: &str) -> Option<Tool> {
+        if let Ok(path_str) = env::var(env) {
+            let path_str = path_str.trim_matches([' ', '"', '\''].as_slice());
+            println!("{env} is set, checking if \"{path_str}\" is a valid tool");
+            let path = PathBuf::from(&path_str);
+
+            if !path.is_file() {
+                panic!("\"{path_str}\" is not a file path.")
+            }
+
+            let output = Command::new(path_str)
+                .arg("--version")
+                .output()
+                .unwrap_or_else(|e| panic!("Failed to run \"{path_str} --version\". ({e})"));
+
+            if output.status.success() {
+                let out_str = String::from_utf8_lossy(&output.stdout);
+                println!("Valid tool found:\n{out_str}");
+            } else {
+                println!("cargo:warning=Tool \"{path_str}\" found, but could not execute \"{path_str} --version\"")
+            }
+
+            return Some(Tool::FullPath(path));
+        }
+
+        println!("{env} not set, looking for {names:?} in PATH");
         for name in names {
             if let Ok(output) = Command::new(name).arg("--version").output() {
                 if output.status.success() {
                     let out_str = String::from_utf8_lossy(&output.stdout);
-                    println!("Valid \"tool\" found:\n{out_str}");
-                    return Some(name);
+                    println!("Valid tool found:\n{out_str}");
+                    return Some(Tool::Name(name));
                 }
             }
         }
 
         None
+    }
+
+    /// Always panics, printing suggestions for finding the specified tool.
+    fn panic_tool_help(name: &str, env: &str, suggestions: &[&str]) {
+        let suggestions_str = if suggestions.is_empty() {
+            String::new()
+        } else {
+            let mut suggestions_str = "For your Operating System we recommend:\n".to_string();
+            for suggestion in &suggestions[..suggestions.len() - 1] {
+                suggestions_str.push_str(&format!("{suggestion}\nOR\n"));
+            }
+            suggestions_str.push_str(suggestions[suggestions.len() - 1]);
+            suggestions_str
+        };
+
+        panic!("No suitable tool equivalent to \"{name}\" has been found in PATH, if one is already installed, either add its directory to PATH or set {env} to its full path. {suggestions_str}")
+    }
+
+    /// Executes [nm][nm] or an equivalent tool in portable mode and returns the output.
+    ///
+    /// ## Panic
+    /// Will panic on any errors.
+    ///
+    /// [nm]: https://www.man7.org/linux/man-pages/man1/nm.1.html
+    fn nm_symbols(tool: &Tool, target_lib: &str, out_path: impl AsRef<Path>) -> String {
+        let output = Command::new(tool.to_string())
+            .current_dir(&out_path)
+            .arg(target_lib)
+            .args(["-p", "-P"])
+            .output()
+            .unwrap_or_else(move |e| panic!("Failed to run \"{tool}\". ({e})"));
+
+        if !output.status.success() {
+            panic!(
+                "An error has occurred while acquiring symbols from the compiled library \"{target_lib}\" ({}):\n{}",
+                output.status,
+                String::from_utf8_lossy(&output.stderr)
+            );
+        }
+
+        String::from_utf8_lossy(&output.stdout).to_string()
+    }
+
+    /// Executes [objcopy][objcopy], adding a prefix to the specified symbols of the target library.
+    ///
+    /// ## Panic
+    /// Will panic on any errors.
+    ///
+    /// [objcopy]: https://www.man7.org/linux/man-pages/man1/objcopy.1.html
+    fn objcopy_redefine(
+        tool: &Tool,
+        target_lib: &str,
+        prefix: &str,
+        symbols: HashSet<&str>,
+        out_path: impl AsRef<Path>,
+    ) {
+        let mut cmd = Command::new(tool.to_string());
+        cmd.current_dir(&out_path);
+        for symbol in symbols {
+            cmd.arg(format!("--redefine-sym={symbol}={prefix}{symbol}"));
+        }
+
+        let output = cmd
+            .arg(target_lib)
+            .output()
+            .unwrap_or_else(move |e| panic!("Failed to run \"{tool}\". ({e})"));
+
+        if !output.status.success() {
+            panic!(
+                "An error has occurred while redefining symbols from library file \"{target_lib}\" ({}):\n{}",
+                output.status,
+                String::from_utf8_lossy(&output.stderr)
+            );
+        }
     }
 
     /// A filter for a symbol in a library.
@@ -238,14 +342,15 @@ mod compat {
     fn get_symbols<'a, const N: usize>(
         nm_output: &'a str,
         filters: [Filter<'a>; N],
-    ) -> impl Iterator<Item = &'a str> + 'a {
-        nm_output
+    ) -> HashSet<&'a str> {
+        let iter = nm_output
             .lines()
             .map(|symbol| {
                 // Strip irrelevant information
 
                 let mut stripped = symbol;
                 while stripped.split(' ').count() > 2 {
+                    // SAFETY: We just made sure ' ' is present above
                     let idx = unsafe { stripped.rfind(' ').unwrap_unchecked() };
                     stripped = &stripped[..idx]
                 }
@@ -263,6 +368,9 @@ mod compat {
                 }
                 false
             })
-            .map(|symbol| &symbol[..symbol.len() - 2]) // Strip the type, so only the symbol remains
+            .map(|symbol| &symbol[..symbol.len() - 2]); // Strip the type, so only the symbol remains
+
+        // Filter duplicates
+        HashSet::from_iter(iter)
     }
 }
